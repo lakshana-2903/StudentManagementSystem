db2a8eead6308783d8620f1f78799cbfc5cc09ed
lakshana-2903 <717824y121@kce.ac.in>
2026-02-23 10:25:48 +0530

    Add Student.java

---
+++ Student.java
@@ -0,0 +1,53 @@
+package com.school.model;
+public class Student {
+    private int id;
+    private String name;
+    private String email;
+    private String course;
+    private float gpa;
+    public Student() {	
+    }
+    public Student(String name, String email, String course, float gpa) {
+        this.name = name;
+        this.email = email;
+        this.course = course;
+        this.gpa = gpa;
+    }
+    public Student(int id, String name, String email, String course, float gpa) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.course = course;
+        this.gpa = gpa;
+    }
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getCourse() {
+        return course;
+    }
+    public void setCourse(String course) {
+        this.course = course;
+    }
+    public float getGpa() {
+        return gpa;
+    }
+    public void setGpa(float gpa) {
+        this.gpa = gpa;
+    }
+}
